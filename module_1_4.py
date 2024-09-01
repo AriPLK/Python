4704601c852cd946464ed55a5f59234df97117d1
AriPLK <art705kukin@gmail.com>
2024-09-01 19:32:19 +0500

    1

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Введите текст: ")
+countString = len(my_string)
+print(f"Кол-во символов в слове {my_string}: {countString}")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
