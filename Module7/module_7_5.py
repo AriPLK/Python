1d80f18075e934a89639c16e2aa9e533e9314393
AriPLK <art705kukin@gmail.com>
2024-10-28 10:00:29 +0500

    1

--- Module7/module_7_5.py
+++ Module7/module_7_5.py
@@ -1,11 +1,15 @@
 import os
+import time
 
-directory = r'C:\Users\rrrrs\PycharmProjects\Python\Module7\second'
-os.walk(directory)
+directory = "."
 
-os.path.join(r'C:\Users\rrrrs\PycharmProjects\Python\Module7',
-             r'C:\Users\rrrrs\PycharmProjects\Python\Module7\second\third')
-
-print(os.path.getmtime(r'C:\Users\rrrrs\PycharmProjects\Python\Module7\module_7_1.py'))
-print(os.path.getsize(r'C:\Users\rrrrs\PycharmProjects\Python\Module7\module_7_1.py'))
-print(os.path.dirname(r'C:\Users\rrrrs\PycharmProjects\Python\Module7\module_7_1.py'))
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.abspath(file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
+            f'Родительская директория: {parent_dir}')
