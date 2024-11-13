de1e9a7af995919d818c68774c16732159667c09
AriPLK <art705kukin@gmail.com>
2024-11-13 11:26:09 +0500

    1

--- Module9/python_9_7.py
+++ Module9/python_9_7.py
@@ -7,14 +7,17 @@ def is_prime(func):
                 is_Prime = False
                 break
         if is_Prime:
-            return (f'Простое\n{func(*args)}')
+            return f'Простое\n{func(*args)}'
         else:
-            return (f'Составное\n{func(*args)}')
+            return f'Составное\n{func(*args)}'
+
     return wrapper
+
 
 @is_prime
 def sum_three(*args):
     return sum(args)
+
 
 result = sum_three(2, 3, 6)
 print(result)
