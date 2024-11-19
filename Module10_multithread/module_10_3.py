dcafd00c2ee6d8c7de28e545b88fe3b8f1aadcd8
AriPLK <art705kukin@gmail.com>
2024-11-19 15:41:07 +0500

    1

--- Module10_multithread/module_10_3.py
+++ Module10_multithread/module_10_3.py
@@ -10,35 +10,28 @@ class Bank:
 
     def deposit(self):
         transactionAmount = 100
-        try:
-            while transactionAmount:
-                topUp = random.randint(50, 500)
-                self.balance += topUp
-                print(f'Пополнение: {topUp}. Баланс: {self.balance}')
-                transactionAmount -= 1
-                time.sleep(0.001)
-                if self.balance >= 500 and lock.locked:
-                    lock.release()
-                    break
-        except Exception:
-            pass
+        while transactionAmount:
+            topUp = random.randint(50, 500)
+            self.balance += topUp
+            print(f'Пополнение: {topUp}. Баланс: {self.balance}')
+            transactionAmount -= 1
+            time.sleep(0.001)
+            if self.balance >= 500 and lock.locked():
+                lock.release()
+
     def take(self):
         transactionAmount = 100
-        try:
-            while transactionAmount:
-                withdraw = random.randint(50, 500)
-                print(f'Запрос на {withdraw}')
-                if withdraw <= self.balance:
-                    self.balance -= withdraw
-                    print(f"Снятие: {withdraw}. Баланс: {self.balance}")
-                    transactionAmount -= 1
-                    time.sleep(0.001)
-                else:
-                    print('Запрос отклонён, недостаточно средств')
-                    lock.acquire()
-                    break
-        except Exception:
-            pass
+        while transactionAmount:
+            withdraw = random.randint(50, 500)
+            print(f'Запрос на {withdraw}')
+            if withdraw <= self.balance:
+                self.balance -= withdraw
+                print(f"Снятие: {withdraw}. Баланс: {self.balance}")
+                transactionAmount -= 1
+                time.sleep(0.001)
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                lock.acquire()
 
 bk = Bank()
 th1 = threading.Thread(target=Bank.deposit, args=(bk,))
