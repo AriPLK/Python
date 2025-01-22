1c48a745dee1bd8f784763d3238f7dd1869bf1b3
AriPLK <art705kukin@gmail.com>
2025-01-23 03:41:38 +0500

    .

--- Module14_database/module_14_2.py
+++ Module14_database/module_14_2.py
@@ -13,27 +13,8 @@ age INTEGER,
 balance INTEGER NOT NULL
 )
 ''')
-# age = 10
-# for i in range(10):
-#     connection.execute(f"INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)",
-#                        (f'User{i+1}', f'example{i+1}@gmail.com', age, f'10000'))
-#     age += 10
-#
-# cursor.execute("SELECT username, age, balance FROM Users")
-# users = cursor.fetchall()
-# for user in users[::2]:
-#     cursor.execute(f"UPDATE Users SET balance = ? WHERE username = ?", ("500", user[0]))
-#
-# for user in users[::3]:
-#     cursor.execute("DELETE FROM Users WHERE username = ?", [user[0]],)
-#
-# cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", ('60',))
-# users = cursor.fetchall()
-# for user in users:
-#     print(user)
 
-#cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
-
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
 cursor.execute("SELECT COUNT(*) FROM Users")
 total_users = cursor.fetchone()[0]
 cursor.execute("SELECT SUM(balance) FROM Users")
